78272d69e8c765b0f427012c5f13997366c7b102
viniolimpio3 <viniolimpio3@gmail.com>
2021-09-18 10:23:07 -0300

    projeto iniciado

---
+++ projetoQuiz_n2_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projetoQuiz_n2_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
